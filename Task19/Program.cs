5f730ec6c148315d399d319219500f4a370500d2
NormaJean13 <navladimirovna@vk.com>
2022-09-12 22:53:12 +0300

    homework3

---
+++ Task19/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 19. Напишите программу, которая принимает на вход пятизначное число
+// и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+Console.Write("Введите пятизначное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+void Palindrom(int n1)
+{
+    while (n1 > -10000 && n1 < 10000 || n1 > 99999 || n1 < -99999)
+    {
+        Console.Write("Введите пятизначное число: ");
+        n1 = Convert.ToInt32(Console.ReadLine());
+    }
+    int first = n1 / 10000;
+    int second = n1 % 10;
+    if (first == second)
+    {
+        first = n1 / 1000 % 10;
+        second = n % 100 / 10;
+        if (first == second) Console.Write($"Число {n1} - палиндром.");
+        else Console.Write($"Число {n1} - не является палиндромом.");
+    }
+    else
+        Console.Write($"Число {n1} - не является палиндромом.");
+}
+Palindrom(n);
---
+++ Task21/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 21
+
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+// A (3,6,8); B (2,1,-7) -> 15.84
+
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+Console.WriteLine("Введите координаты точки ");
+Console.Write ("x: ");
+double ax = Convert.ToDouble(Console.ReadLine());
+Console.Write ("y: ");
+double ay = Convert.ToDouble(Console.ReadLine());
+Console.Write ("z: ");
+double az = Convert.ToDouble(Console.ReadLine());
+Console.Write ("x: ");
+double bx = Convert.ToDouble(Console.ReadLine());
+Console.Write ("y: ");
+double by = Convert.ToDouble(Console.ReadLine());
+Console.Write ("z: ");
+double bz = Convert.ToDouble(Console.ReadLine());
+
+
+double Result (double ax1, double ay1, double bx1, double by1, double az1, double bz1)
+{
+    return Math.Sqrt((bx1 - ax1)*(bx1 - ax1) + (by1 - ay1)*(by1 - ay1) + (bz1 - az1)*(bz1 - az1));
+}
+double result = Result (ax, ay, bx, by, az, bz);
+Console.WriteLine(Math.Round (result,2, MidpointRounding.ToZero));
